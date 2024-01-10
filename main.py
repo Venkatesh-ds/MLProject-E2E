07bb04c7533cb2bb851e0e6a8c2d3efed00448f0
Venkatesh19860924 <venkateshdsprof@gmail.com>
2024-01-10 10:30:21 +0530

    removed spaces in main file

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from mlProject.pipeline.stage_03_data_transformation import DataTransformationTr
 from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
-
-
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
